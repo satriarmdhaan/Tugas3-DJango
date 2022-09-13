cc5cdfb655c68b8bfb845f9af669517bb2df053d
Muhammad Satria Ramadhan <muhammad.satria12@ui.ac.id>
2022-09-14 01:30:16 +0700

    initial commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_katalog,
+        'nama': 'Muhammad Satria Ramadhan',
+        'student_id': '2106751695'
+    }
+    return render(request, "katalog.html", context)
 # TODO: Create your views here.
