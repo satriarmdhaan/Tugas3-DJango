7a4bf4bcf60dc6de36836458d121f7700d3f2eb3
Muhammad Satria Ramadhan <muhammad.satria12@ui.ac.id>
2022-09-21 17:23:09 +0700

    initial commit

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -2,14 +2,12 @@ from django.test import TestCase, Client
 from django.urls import resolve
 
 class AppTest(TestCase):
-    def app_url_html(self):
-        response = Client().get('https://localhost:8000/mywatchlist/html/')
+    def test_app_url_html(self):
+        response = Client().get('/mywatchlist/html/')
         self.assertEqual(response.status_code,200)
-    
-    def app_url_xml(self):
-        response = Client().get('https://localhost:8000/mywatchlist/xml/')
+    def test_app_url_xml(self):
+        response = Client().get('/mywatchlist/xml/')
         self.assertEqual(response.status_code,200)
-    
-    def app_url_json(self):
-        response = Client().get('https://localhost:8000/mywatchlist/json/')
+    def test_app_url_json(self):
+        response = Client().get('/mywatchlist/json/')
         self.assertEqual(response.status_code,200)
